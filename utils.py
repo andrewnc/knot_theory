cb9eab933489cc269f8d3d2519caf6423547e64b
Andrew Carr <andrewcarr06@gmail.com>
2020-01-31 13:55:37 -0700

    update

--- utils.py
+++ utils.py
@@ -1,15 +1,13 @@
 import pandas as pd
+import torch
+import numpy as np
 from typing import List
-import sys
-
-file_name = sys.argv[1]
-
-df = pd.read_csv(file_name)
-
-print(df)
+import os
+import pathlib
+from tqdm import tqdm
 
 
-def string_to_dt(code: str) -> List:
+def decode_dt(code: str) -> List:
     # capital letters are 65 - 90 inclusive
     # lower case are 97 - 122 inclusive
     capital_letters = [chr(i) for i in range(65, 91)]
@@ -33,13 +31,54 @@ def string_to_dt(code: str) -> List:
 
     return dt_code
 
-def jones_to_str(poly: List) -> str:
-    jones_str = ""
+def poly_to_str(poly: List) -> str:
+    out_str = ""
     for i, (coeff, exp) in enumerate(poly):
-        jones_str += f" {coeff}x^{exp} "
+        out_str += f" {coeff}x^{exp} "
         if i != (len(poly) - 1):
-            jones_str += "+"
-    return jones_str
+            out_str += "+"
+    return out_str
 
-print(string_to_dt(df.iloc[0]["dt_code"]))
-print(jones_to_str(eval(df.iloc[0]["Jones_polynomial"])))
+def poly_representation_padding(df: pd.DataFrame) -> [List, List]:
+    """This function is mostly for reproducability purposes. There isn't much chance it will get used again"""
+
+    out = []
+    for poly_name in ['Jones_polynomial', 'Alexander_polynomial']:
+        poly = df[poly_name].apply(lambda x: eval(x)).values
+        poly_list = []
+        for i in poly:
+            poly_list.append(torch.tensor(list(i)))
+        out.append(torch.nn.utils.rnn.pad_sequence(poly_list))
+
+    return out
+
+
+
+def put_str_repr_on_csv():
+    for file_name in tqdm(os.listdir(".")):
+        file = pathlib.Path(f"./{file_name}")
+        if file.suffix == ".csv":
+            df = pd.read_csv(file_name)
+            df['jones_str'] = df['Jones_polynomial'].apply(lambda x: eval(x)).transform(lambda x: poly_to_str(x))
+            df['alexander_str'] = df['Alexander_polynomial'].apply(lambda x: eval(x)).transform(lambda x: poly_to_str(x))
+            df.to_csv(f"{file.stem}_str.csv", index=False)
+
+def format_csv_to_txt_for_openmt(file_name):
+    df = pd.read_csv(file_name)
+    file = pathlib.Path(f"./{file_name}")
+    with open(f"./data/{file.stem}_jones_tgt.txt", "w") as f:
+        for poly in tqdm(df['jones_str']):
+            f.write(poly)
+            f.write("\n")
+    with open(f"./data/{file.stem}_dt_src.txt", "w") as f:
+        for code in tqdm(df['dt_code']):
+            f.write(code)
+            f.write("\n")
+
+if __name__ == "__main__":
+    for file in os.listdir("."):
+        file = pathlib.Path(f"./{file}")
+        if file.suffix == ".csv" and "str" in file.stem:
+            format_csv_to_txt_for_openmt(file)
+
+
