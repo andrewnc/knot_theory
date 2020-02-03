8e697cd5bf76c57b2c6ebde85bb148b9a4dc824f
Chris Rytting <christophermichaelrytting@gmail.com>
2020-02-03 11:29:33 -0700

    Added spacing between what we want to be tokens

--- utils.py
+++ utils.py
@@ -2,9 +2,11 @@ import pandas as pd
 import torch
 import numpy as np
 from typing import List
+import ast
 import os
 import pathlib
 from tqdm import tqdm
+
 
 
 
@@ -32,10 +34,14 @@ def decode_dt(code: str) -> List:
 
     return dt_code
 
+def polystr_to_polylist(polystr: str) -> List:
+    polylist = ast.literal_eval(polystr)
+    return polylist
+
 def poly_to_str(poly: List) -> str:
     out_str = ""
     for i, (coeff, exp) in enumerate(poly):
-        out_str += f" {coeff}x^{exp} "
+        out_str += f" {coeff} x^ {exp} "
         if i != (len(poly) - 1):
             out_str += "+"
     return out_str
@@ -56,6 +62,12 @@ def poly_representation_padding(df: pd.DataFrame) -> [List, List]:
 
 
 def put_str_repr_on_csv():
+    """
+    For all the .csv files in this directory, Take the Jones and Alexander
+    polynomial list representations and change them to string representations.
+    The file that is produced is a dataframe with these string representations
+    appended to the original file.
+    """
     for file_name in tqdm(os.listdir(".")):
         file = pathlib.Path(f"./{file_name}")
         if file.suffix == ".csv":
@@ -67,17 +79,19 @@ def put_str_repr_on_csv():
 def format_csv_to_txt_for_openmt(file_name):
     df = pd.read_csv(file_name)
     file = pathlib.Path(f"./{file_name}")
-    with open(f"./data/{file.stem}_jones_tgt.txt", "w") as f:
+    with open(f"./data/{file.stem}_tgt.txt", "w") as f:
         for poly in tqdm(df['jones_str']):
             f.write(poly)
             f.write("\n")
-    with open(f"./data/{file.stem}_dt_src.txt", "w") as f:
+    with open(f"./data/{file.stem}_src.txt", "w") as f:
         for code in tqdm(df['dt_code']):
+            code = " ".join([char for char in code])
             f.write(code)
             f.write("\n")
 
 if __name__ == "__main__":
     for file in os.listdir("."):
+        print('ehllo')
         file = pathlib.Path(f"./{file}")
         if file.suffix == ".csv" and "str" in file.stem:
             format_csv_to_txt_for_openmt(file)
